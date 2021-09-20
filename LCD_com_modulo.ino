c59b028fbeef70c839f9821df200b0957514898d
paulocoppe10 <79857716+paulocoppe10@users.noreply.github.com>
2021-09-20 14:16:59 -0300

    Add files via upload

---
+++ LCD_com_modulo.ino
@@ -0,0 +1,20 @@
+//Programa: Display LCD 16x2 e modulo I2C
+//Autor: Arduino e Cia
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+//Inicializa o display no endereco 0x27
+LiquidCrystal_I2C lcd(0x27,16,2);
+ 
+void setup()
+{
+ lcd.init();
+}
+ 
+void loop()
+{
+  lcd.setBacklight(HIGH);
+  lcd.setCursor(0,0);
+  lcd.print("ele fica assim");
+  lcd.setCursor(0,1);
+  lcd.print("com o modulo I2C");
+}
